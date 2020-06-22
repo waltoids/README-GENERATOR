a35f4e3395af0bec83f5a0c7c2e1bfffec7bb14b
Walter Joy IV <waltoids@users.noreply.github.com>
2020-06-22 19:13:59 -0400

    added markdown data to return to init function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,42 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+# Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description:
+![License](https://img.shields.io/badge/License-${data.license}-brightgreen.svg)
+
+${data.description}
+
+## Installation:
+${data.installation}
+
+## Usage:
+${data.usage}
+
+## License:
+
+    For more info about the license, follow the link below.
+    [License](https://opensource.org/licenses/${data.license})
+
+## Contributing:
+${data.contributing}
+
+## Tests:
+${data.tests}
+
+## Questions?:
+
+   Contact me at: ${data.email} for questions.
+
+
 `;
 }
 
